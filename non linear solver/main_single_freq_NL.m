e4b491d19c8b5f0fbab3cd00c2d85b98fd0956d0
Guy Pardo <guy.pardo@gmail.com>
2021-08-23 10:06:30 +0300

    doc

--- non linear solver/main_single_freq_NL.m
+++ non linear solver/main_single_freq_NL.m
@@ -7,11 +7,11 @@ addpath(genpath('.\')); % adding subfolders of current folder (assuming current 
 freq = 8.5e9;
 input_idx = 4;
 PMT2_flag = false; % set true for 2 traces network, false for 7 traces network
-input_pwr = -50; % in dbm
+input_pwr = -46; % in dbm
 %input_pwr =  11.402714422492686; % in dBm. this value give voltage amplitude of 1 Volt
 iterations = 30; % for NL solution
-plot_iterations = true;
-network_plots=true; % network plots that include also the couplers. 
+plot_iterations = true; % set to true to look at the graph and check convergence. (automatic convergence check is NOT implemented yet.)
+network_plots=false; % network plots that include also the couplers. 
 %% define graph
 if PMT2_flag
     nockit_params = get_nockit2_params();
